f62c8e8484d583a35c9388ae95b84a536b34337e
NAND7095 <duhgfhfdghg@gmail.com>
2025-03-23 21:39:22 +0530

    Update __init__.py

--- RISHUCHATBOT/__init__.py
+++ RISHUCHATBOT/__init__.py
@@ -1,16 +1,12 @@
 import logging
 import time
 from pymongo import MongoClient
-from Abg import patch
-from RISHUCHATBOT.userbot.userbot import Userbot
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import config
 import uvloop
-import time
-ID_CHATBOT = None
-CLONE_OWNERS = {}
+
 uvloop.install()
 
 logging.basicConfig(
@@ -23,56 +19,11 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 boot = time.time()
+
 mongodb = MongoCli(config.MONGO_URL)
 db = mongodb.Anonymous
 mongo = MongoClient(config.MONGO_URL)
-OWNER = config.OWNER_ID
-_boot_ = time.time()
-clonedb = None
-def dbb():
-    global db
-    global clonedb
-    clonedb = {}
-    db = {}
 
-cloneownerdb = db.clone_owners
-
-async def load_clone_owners():
-    async for entry in cloneownerdb.find():
-        bot_id = entry["bot_id"]
-        user_id = entry["user_id"]
-        CLONE_OWNERS[bot_id] = user_id
-
-async def save_clonebot_owner(bot_id, user_id):
-    await cloneownerdb.update_one(
-        {"bot_id": bot_id},
-        {"$set": {"user_id": user_id}},
-        upsert=True
-    )
-async def get_clone_owner(bot_id):
-    data = await cloneownerdb.find_one({"bot_id": bot_id})
-    if data:
-        return data["user_id"]
-    return None
-
-async def delete_clone_owner(bot_id):
-    await cloneownerdb.delete_one({"bot_id": bot_id})
-    CLONE_OWNERS.pop(bot_id, None)
-
-async def save_idclonebot_owner(clone_id, user_id):
-    await cloneownerdb.update_one(
-        {"clone_id": clone_id},
-        {"$set": {"user_id": user_id}},
-        upsert=True
-    )
-
-async def get_idclone_owner(clone_id):
-    data = await cloneownerdb.find_one({"clone_id": clone_id})
-    if data:
-        return data["user_id"]
-    return None
-
-    
 class RISHUCHATBOT(Client):
     def __init__(self):
         super().__init__(
@@ -118,5 +69,3 @@ def get_readable_time(seconds: int) -> str:
     return ping_time
 
 RISHUCHATBOT = RISHUCHATBOT()
-userbot = Userbot()
-
